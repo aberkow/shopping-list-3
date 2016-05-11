18855595842ae7c3f790e7b3e4285ad9064c86e8
aberkow <berkowitz.clarinet@gmail.com>
2016-05-10 20:38:05 -0400

    changes to delete function

--- js/server.js
+++ js/server.js
@@ -4,9 +4,23 @@ var jsonParser = bodyParser.json();
 //debugger;
 
 var Storage = function(){
+  //creating one object suggested by Jim C. See note below
+  // this.items = {
+  //   0: "carrots",
+  //   1: "something"
+  // };
   this.items = [];
+  console.log(this.items);
   this.id = 0;
 };
+//empty string might be needed here but not on line 18
+// this.items[this.id + ''] = "something"
+// delete this.items[id];
+
+//var index = this.items[index]
+
+var list = [];
+
 
 Storage.prototype.add = function(name){
   //debugger;
@@ -16,17 +30,28 @@ Storage.prototype.add = function(name){
   };
   this.items.push(item);
   this.id += 1;
+  console.log(item);
+  console.log("item list " + this.items);
   return item;
 };
 
 //takes in the id of an item and uses splice to remove/return the new array.
-Storage.prototype.delete = function(id){
-  // var item = {
-  //   id: this.id
-  // };
-
-  this.items.splice(id, 1);
-  return this.items;
+Storage.prototype.delete = function(idToRemove){
+  var indexOfIDToRemove;
+  var hasFoundItem = false;
+  this.items.some(function (item, index, array){
+    if (item.id === idToRemove){
+      indexOfIDToRemove = index;
+      hasFoundItem = true;
+      return true;
+    }
+  });
+  if (hasFoundItem){
+    this.items = list.slice(0, indexOfIDToRemove).concat(list.slice(indexOfIDToRemove + 1));
+  }
+  return hasFoundItem;
+  //console.log(id);
+  //return this.items;
 };
 
 var storage = new Storage();
@@ -58,15 +83,18 @@ app.post('/items', jsonParser, function(request, result){
 //this function uses storage.delete as a callback. storage.delete wants an id as
 //an argument. maybe use the hello server model to get the value of the id property?
 //right now this deletes the first item in the list but not any random item selected.... getting closer.
-app.delete('/items/:id', jsonParser, function(request, result){
+app.delete('/items/:id', jsonParser,
+function(request, result){
   if (!request.body){
     return result.sendStatus(400);
   }
-
   var idOfItem = request.params.id;
+  console.log("id of item " + idOfItem);
+  return result.status(storage.delete(idOfItem) ? 200 : 404);
 
-  var itemToDelete = storage.delete(idOfItem);
-  result.status(200).json(itemToDelete);
+
+  // var itemToDelete = storage.delete(idOfItem);
+  // result.status(200).json(itemToDelete);
 });
 
 app.listen(4000, 'localhost', function(){
